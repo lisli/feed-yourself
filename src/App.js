9243a90449359aefc7523d2e59be2eae8cf00335
Lisa Yoder <lisa.alane.yoder@gmail.com>
2017-04-03 22:00:12 -0400

    fetch food from google doc

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const FOOD_URL = 'https://spreadsheets.google.com/feeds/list/1czrNQfMugAcw7OyDCGwQ0v0vXrIBsuxfFCQlVDhFwLg/od6/public/values?alt=json';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { data: null };
+  }
+
+  componentDidMount() {
+    fetch(FOOD_URL)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({data: json});
+      });
+  }
+
   render() {
+    const { data } = this.state;
+    console.log({data});
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <pre><code>{JSON.stringify(data)}</code></pre>
       </div>
     );
   }
